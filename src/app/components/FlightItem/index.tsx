ade59780a4abac34ca085c81af170eb9bf51535c
grisha <gmamontov@alt-lan.ru>
2017-06-20 18:26:53 +0300

    flexbox

--- src/app/components/FlightItem/index.tsx
+++ src/app/components/FlightItem/index.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react';
-import { IFlight } from 'models/avia';
+import * as moment from 'moment';
 
+import { IFlight } from 'models/avia';
 const style = require('./style.css');
 
 export const FlightItem = (props: { flight: IFlight }) => (
     <div className={style.FlightItem}>
-        <div>
-            <h4>{props.flight.carrier}</h4>
-            <div>{props.flight.direction.from} - {props.flight.direction.to}</div>
-            <div>{props.flight.arrival} - {props.flight.departure}</div>
+        <h4 className={style.title}>{props.flight.carrier}</h4>
+        <div className={style.text}>
+            <h5>{props.flight.direction.from}<span> - </span>{props.flight.direction.to}</h5>
+            <div>
+                Туда, {moment(props.flight.arrival).format('DD.MM.YYYY')}
+            </div>
+            <div>
+                <strong>
+                    {moment(props.flight.arrival).format('HH:mm:ss')}
+                    <span> - </span>
+                    {moment(props.flight.departure).format('HH:mm:ss')}
+                </strong>
+            </div>
         </div>
     </div>
 );
